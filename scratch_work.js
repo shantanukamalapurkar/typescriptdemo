936617f86294e8bb65b12759675db99720e722fa
shantanu kamalapurkar <shantanu.kamalapurkar@gmail.com>
2022-05-22 01:39:26 -0400

    added

--- scratch_work.js
+++ scratch_work.js
@@ -1,3 +1,5 @@
 console.log("hello world");
 console.log("Hey there");
 console.log("On local branch");
+console.log("I want to see some conflict in merging");
+console.log("Hope to see all the changes");
--- scratch_work.ts
+++ scratch_work.ts
@@ -1,4 +1,5 @@
 console.log("hello world");
 console.log("Hey there");
 console.log("On local branch");
-console.log("Made some changes");
+console.log("I want to see some conflict in merging");
+console.log("Hope to see all the changes");
